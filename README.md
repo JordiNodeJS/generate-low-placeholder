e6f9229f25f7f28089b49198f533d006b591ccf9
JORDI.NODEJS <jordi.nodejs@gmail.com>
2023-02-22 22:14:32 +0100

    tailwind order plugin prettier

---
+++ README.md
@@ -0,0 +1,11 @@
+# [CloudCreate Hackathon Submission](https://forms.monday.com/forms/bc9e0eefc52aca8b49b1fcae951eb910?r=use1)
+
+### CÓMO PARTICIPAR
+1. Regístrate en https://cloudinary.com/
+2. Deja tu proyecto en este hilo: POR CREAR
+3. Envía tu proyecto a este formulario: https://forms.monday.com/forms/bc9e0eefc52aca8b49b1fcae951eb910?r=use1
+
+⚠️ FECHA MÁXIMA DE ENTREGA: 6 Marzo 23:59:59 CET
+
+CONSEGUIR MÁS CRÉDITOS GRATIS 
+https://cld.media/cloudcreateregister
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import SvgComponent from './components/Cloudinary'
+
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="m-auto grid h-screen w-full max-w-xl place-content-center">
+      <header className="flex justify-center py-10">
+        <h1 className='text-3xl font-bold text-blue-900 tracking-tighter'>
+          Remove<span className='text-blue-600'>bg</span>
+        </h1>
+      </header>
+      <main></main>
+      <footer className="flex items-center justify-center gap-x-2 font-semibold">
+        Made with <SvgComponent className="w-24" />
+      </footer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
