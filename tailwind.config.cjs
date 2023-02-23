fe6d0834dd0e766909aa886c758e4f117f704177
JORDI.NODEJS <jordi.nodejs@gmail.com>
2023-02-23 20:26:45 +0100

    add personal config tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [require("daisyui")],
-};
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#1637c9',
+          secondary: '#2f378c',
+          accent: '#ed78c8',
+          neutral: '#191D24',
+          'base-100': '#41365E',
+          info: '#84C7DB',
+          success: '#186D41',
+          warning: '#CA9A16',
+          error: '#ED1216'
+        }
+      }
+    ]
+  },
+  plugins: [require('daisyui')]
+}
