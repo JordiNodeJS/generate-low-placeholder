40cec8310c78a4f59c484840e2bbb460e73f6686
JORDI.NODEJS <jordi.nodejs@gmail.com>
2023-02-24 16:41:29 +0100

    styles

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import TestReducer from './components/TestReducer'
 
 function App() {
   return (
-    <div className="grid w-full h-screen max-w-xl grid-cols-1 m-auto place-content-center">
+    <div className="m-auto grid h-screen w-full max-w-xl grid-cols-1 place-content-center">
       <TestReducer />
       <header className="flex justify-center py-10">
         <h1 className="text-3xl font-bold tracking-tighter text-blue-900">
@@ -13,11 +13,11 @@ function App() {
         </h1>
       </header>
       <main>
-        <h1 className="text-center">Drag and Drop Test</h1>
-      <CloudinaryUploadWidget />
+        <h1 className="text-center text-5xl font-bold">Drag and Drop Test</h1>
+        <CloudinaryUploadWidget />
         {/* <DropzoneImage /> */}
       </main>
-      <footer className="flex items-center justify-center font-semibold gap-x-2">
+      <footer className="flex items-center justify-center gap-x-2 font-semibold">
         Made with <SvgComponent className="w-24" />
       </footer>
     </div>
