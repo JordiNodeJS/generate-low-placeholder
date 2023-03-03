c7bf80a5df15ce5d0152f77d7673a6d510f37317
JORDI.NODEJS <jordi.nodejs@gmail.com>
2023-03-03 02:11:06 +0100

    extract handleDrop

--- src/components/CloudinaryUpload.jsx
+++ src/components/CloudinaryUpload.jsx
@@ -18,6 +18,7 @@ import {
 import { useMyContext } from '../store/context'
 import { Actions } from '../store/reducer'
 import TwoUp from './TwoUp'
+import { handleDrop } from '../utils/handleDrop'
 
 const CloudinaryUpload = () => {
   const { state, dispatch } = useMyContext()
@@ -31,48 +32,48 @@ const CloudinaryUpload = () => {
     })
   }, [])
 
-  const handleDrop = async acceptedFiles => {
-    dispatch({ type: Actions.SET_IS_UPLOADING, payload: { isUpLoading: true } })
-    const file = acceptedFiles[0]
-    const formData = new FormData()
-    formData.append('file', file)
-    formData.append('upload_preset', import.meta.env.VITE_UPLOAD_PRESET)
-    formData.append('timestamp', Date.now() / 1000)
-    try {
-      const response = await fetch(
-        `https://api.cloudinary.com/v1_1/${
-          cld.getConfig().cloud.cloudName
-        }/image/upload`,
-        {
-          method: 'POST',
-          body: formData
-        }
-      )
-      const data = await response.json()
-      dispatch({
-        type: Actions.SET_CLOUDINARY_IMG,
-        payload: { cldImage: data }
-      })
+  // const handleDrop = async acceptedFiles => {
+  //   dispatch({ type: Actions.SET_IS_UPLOADING, payload: { isUpLoading: true } })
+  //   const file = acceptedFiles[0]
+  //   const formData = new FormData()
+  //   formData.append('file', file)
+  //   formData.append('upload_preset', import.meta.env.VITE_UPLOAD_PRESET)
+  //   formData.append('timestamp', Date.now() / 1000)
+  //   try {
+  //     const response = await fetch(
+  //       `https://api.cloudinary.com/v1_1/${
+  //         cld.getConfig().cloud.cloudName
+  //       }/image/upload`,
+  //       {
+  //         method: 'POST',
+  //         body: formData
+  //       }
+  //     )
+  //     const data = await response.json()
+  //     dispatch({
+  //       type: Actions.SET_CLOUDINARY_IMG,
+  //       payload: { cldImage: data }
+  //     })
 
-      dispatch({
-        type: Actions.SET_PUBLIC_ID,
-        payload: { publicId: data.public_id }
-      })
-      dispatch({
-        type: Actions.SET_IS_UPLOADING,
-        payload: { isUpLoading: false }
-      })
-    } catch (error) {
-      // eslint-disable-next-line no-console
-      console.error(error)
-    }
-  }
+  //     dispatch({
+  //       type: Actions.SET_PUBLIC_ID,
+  //       payload: { publicId: data.public_id }
+  //     })
+  //     dispatch({
+  //       type: Actions.SET_IS_UPLOADING,
+  //       payload: { isUpLoading: false }
+  //     })
+  //   } catch (error) {
+  //     // eslint-disable-next-line no-console
+  //     console.error(error)
+  //   }
+  // }
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     noClick: false,
     maxFiles: 1,
     accept: { 'image/*': [] },
-    onDrop: handleDrop
+    onDrop: acceptedFiles => handleDrop(acceptedFiles, dispatch)
   })
 
   return (
