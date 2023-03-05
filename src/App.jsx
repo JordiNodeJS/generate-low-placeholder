ce9dbb49bb1db2d96ab1ac51aa1b4bd221de0799
JORDI.NODEJS <jordi.nodejs@gmail.com>
2023-03-05 17:20:49 +0100

    fix padding in header

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <DarkModeToggle />
       {/* <TestReducer /> */}
       <header className="flex justify-center py-10" title='Generate low quality image placeholders'>
-        <h1 className="bg-gradient-to-r from-purple-400 to-pink-600 bg-clip-text text-center text-4xl font-extrabold tracking-tighter text-transparent">
+        <h1 className="pr-2 bg-gradient-to-r from-purple-400 to-pink-600 bg-clip-text text-center text-4xl font-extrabold tracking-tighter text-transparent">
           Generate
           <span className="text-2xl font-extrabold text-purple-300">low quality</span>
           <span className="">image</span> <br />
